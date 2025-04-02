44acb02ade3b47deca8628af1505882d3a813615
RolanWebT <rolan.web.t@gmail.com>
2025-04-02 14:11:33 +0200

    :construction: Arrays begin

---
+++ Basics/Lists.py
@@ -0,0 +1,35 @@
+"""
+There are 4 built-in data types in 
+Python used to store collections of data:
+    1. List: ordered and changeable. Allows duplicate members.
+    2. Tuple: which is ordered and unchangeable. Allows duplicate members.
+    3. Set: unordered, unchangeable*, and unindexed. No duplicate members.
+    4. Dictionary: ordered(V 3.7) and changeable. No duplicate members.
+"""
+
+# List 
+myList = ["banana", "apple", "cherry", "orange", "kiwi", "melon", "mango"]
+print(myList)
+print("myList[1]: ", myList[1])
+
+# len() function
+print("Has the length:", len(myList))
+
+print("Type:", type(myList))
+
+# List Constructor: list()
+
+# Check if Item Exists
+# if "item" in myList:
+#    print
+if "apple" in myList:
+    print("Yes, Apple is in the List")
+
+
+""" Lists Methods: """
+# insert(index, "item"): 
+# insert an item in a specific index
+
+
+# Tuple
+
