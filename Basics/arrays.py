018ee034614f925c8238af055f4b6d9cfe0ec94d
RolanWebT <rolan.web.t@gmail.com>
2025-04-02 11:00:05 +0200

    first commit

---
+++ Basics/arrays.py
@@ -0,0 +1,19 @@
+"""
+There are 4 built-in data types in 
+Python used to store collections of data:
+    1. List
+    2. Tuple
+    3. Set
+    4. Dictionary
+"""
+
+# List 
+myList = ["banana", "apple"]
+print(myList)
+print("myList[1]: ", myList[1])
+print("Has the length:", len(myList))
+print("Type:", type(myList))
+
+# List Constructor: list()
+
+
