8ceb84bb938b7e9159b9ae53d95a25e1bcc0ef0a
RolanWebT <rolan.web.t@gmail.com>
2025-04-04 15:02:10 +0200

    :memo: new Methods to deal with Lists

--- Basics/Lists.py
+++ Basics/Lists.py
@@ -9,7 +9,7 @@ Python used to store collections of data:
 
 # List 
 myList = ["banana", "apple", "cherry", "orange", "kiwi", "melon", "mango"]
-print(myList)
+print("myList:\n",myList)
 print("myList[1]: ", myList[1])
 
 # len() function
@@ -25,11 +25,43 @@ print("Type:", type(myList))
 if "apple" in myList:
     print("Yes, Apple is in the List")
 
+# Change items in a List
+myList[1:3] = ["new", "new"]
+print("\n changed List [1:3]:\n ",myList)
+# NOTICE 
+# myList[1:2] = ["blackcurrant", "watermelon"] 
+# will change the second item with two new items!!
 
-""" Lists Methods: """
-# insert(index, "item"): 
+""" Add items to a List Methods: """
+# 1. insert(index, "item"): 
+
 # insert an item in a specific index
+myList.insert(2, "watermelon")
+print("\n insert a new item in the third index:\n", myList)
+
+# 2. append("item")
+
+# 3. extend(another list)
+newList = ["mango", "papaya"]
+myList.extend(newList)
+print("\n Extend a List:\n", myList)
 
 
-# Tuple
+""" Remove From a List """
+# 1. .remove("item") // will remove ONLY the first matched item
+myList.remove("new")
+print("\n Remove first 'new':\n", myList)
+# 2. .pop(index)
+myList.pop(1)
+print("\n Remove the second item [1]:\n", myList)
+#  .pop() remove the last item
+myList.pop()
+print("\n Remove the last item:\n", myList)
 
+# 3. del Keyword
+del myList[3] # delete the fourth item
+print("\n delete the fourth item with 'del' :\n", myList)
+del myList # delete the entire List 
+# print(type(myList))
+
+# 4. .Clear() will clear the List but it still remain
